e6f1849f438e9fcab88539dfe4d62266471afc26
Rony Cesana <rony.cesana@gmail.com>
2017-03-28 12:17:51 +0200

    Creazione primo scheletro Spring Boot

---
+++ src/main/java/npe/ZiaMafaldaApplication.java
@@ -0,0 +1,12 @@
+package npe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZiaMafaldaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZiaMafaldaApplication.class, args);
+	}
+}
