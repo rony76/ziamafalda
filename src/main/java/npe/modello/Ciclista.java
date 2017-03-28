5c7948e61c100261da5a98137a2588cf0256ec51
Rony Cesana <rony.cesana@gmail.com>
2017-03-28 14:11:09 +0200

    Salvataggio nuovo Ciclista

---
+++ src/main/java/npe/modello/Ciclista.java
@@ -0,0 +1,51 @@
+package npe.modello;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "ciclisti")
+public class Ciclista {
+    private Long id;
+    private String nome;
+    private String cognome;
+    private String telefono;
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @NotNull
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @NotNull
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    @NotNull
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+}
---
+++ src/main/java/npe/modello/CiclistaRepository.java
@@ -0,0 +1,6 @@
+package npe.modello;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CiclistaRepository extends CrudRepository<Ciclista, Long> {
+}
